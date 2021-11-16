207d12a4a3366bcb1b2429045a9354d4f59136a9
Lan Hui <1348141770@qq.com>
2021-11-16 22:20:40 +0800

    LAMASHEVSKYI DMYTRO's work on mobile phone UI adaption.  Need more detail?  Check the Kanban card at http://118.25.96.118/kanboard/?controller=TaskViewController&action=show&task_id=1483&project_id=170

--- Footer.php
+++ Footer.php
@@ -1,4 +1,9 @@
-<br><br><br><br><br><br><br><br><br>
-<div style="background-color:white;width:100%di">
-
-</div>
+ 
+ <footer class="footer bg-primary">
+      <div class="container text-light"> 
+        <p >LRR was originally developed as a <a href="http://lanlab.org/course/2018f/se/homepage.html" style="color:white;">Software engineering course project</a> by Mohamed Nor and Elmahdi Houzi.  Please submit your suggestions or bug reports to  lanhui _at_ zjnu.edu.cn.  Last updated on 18/04/2020 by Ashly. <a href="./homepage" style="color:white;">More information ...</a></p> 
+      <small>© 2018 Copyright: <a href="http://lanlab.org/">lanlab.org</a></small>
+      </div>
+    </footer>
+  
+ 
